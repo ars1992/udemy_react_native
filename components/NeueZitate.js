7b7cde5ca245b438fd441706ffab499f7b9ffd8f
AlessandroSalomonMac <>
2023-12-14 17:27:09 +0100

    ende 56 inputText

--- components/NeueZitate.js
+++ components/NeueZitate.js
@@ -1,12 +1,13 @@
-import { Modal, Pressable, Text, StyleSheet, View } from "react-native";
+import { Modal, Pressable, TextInput, Text, StyleSheet, View } from "react-native";
 
 
 export default function neuesZitat({ visible, onCancel }) {
     return (
         <Modal visible={visible} onRequestClose={onCancel}>
             <View style={styles.container}>
-                <Text>hallo neu</Text>
-                <Pressable onPress={onCancel}>
+                <TextInput placeholder="Zitat" style={styles.input}></TextInput>
+                <TextInput placeholder="Autor" style={styles.input}></TextInput>
+                <Pressable style={[styles.button, styles.abbbrechen]} onPress={onCancel}>
                     <Text>abbrechen</Text>
                 </Pressable>
             </View>
@@ -19,5 +20,25 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    input: {
+        borderWidth: 2,
+        borderRadius: 5,
+        width: "80%",
+        padding: 10,
+        marginBottom: 10,
+        fontSize: 25,
+
+    },
+    button: {
+        position: 'absolute',
+        bottom: 50,
+        borderWidth: 1,
+        borderRadius: 10,
+        backgroundColor: "cornflowerblue",
+        padding: 10,
+    },
+    abbbrechen: {
+        right: 20,
+    },
 })
