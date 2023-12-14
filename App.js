9c53b42059ddc0eaac5f658cd5b07be131e6efc7
AlessandroSalomonMac <>
2023-12-14 13:31:01 +0100

    ende 47 style pressable

--- App.js
+++ App.js
@@ -20,10 +20,10 @@ export default function App() {
       <Zitate text={zitat.text} autor={zitat.autor}/>
 
       <Pressable style={[styles.button, styles.next]} onPress={() => setIndex((index + 1) % zitate.length)}>
-        <Text>next</Text>
+        <Text style={styles.buttonText}>next</Text>
       </Pressable>
       <Pressable style={[styles.button, styles.back]} onPress={() => setIndex(index == 0 ? zitate.length - 1: index - 1)}>
-        <Text>back</Text>
+        <Text style={styles.buttonText}>back</Text>
       </Pressable>
       <StatusBar style="auto" />
     </View>
@@ -40,6 +40,13 @@ const styles = StyleSheet.create({
   button: {
     position: 'absolute',
     bottom: 50,
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: "cornflowerblue",
+    padding: 10,
+  },
+  buttonText: {
+    fontSize: 25,
   },
   next: {
     left: 20,
