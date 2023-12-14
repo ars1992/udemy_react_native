888f714884889ac66741c5d0031c416b595a1e56
AlessandroSalomonMac <>
2023-12-14 13:10:12 +0100

    ende 45 pressable

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { useState } from 'react';
 import Zitate from './components/Zitate'
 
@@ -14,18 +14,13 @@ export default function App() {
 
   const [index, setIndex] = useState(0)
   const zitat = zitate[index]
-  // const useStates = useState(0)
-  // const index = useStates[0]
-  // console.log(useStates)
-  // const zitat = zitate[index]
-  // const setIndex = useStates[1]
 
   return (
     <View style={styles.container}>
       <Zitate text={zitat.text} autor={zitat.autor}/>
 
-      <Button title='next' onPress={() => setIndex((index + 1) % zitate.length)}></Button>
-      <Button title='back' onPress={() => setIndex(index == 0 ? zitate.length - 1: index - 1)}></Button>
+      <Pressable onPress={() => setIndex((index + 1) % zitate.length)}><Text>next</Text></Pressable>
+      <Pressable onPress={() => setIndex(index == 0 ? zitate.length - 1: index - 1)}><Text>back</Text></Pressable>
       <StatusBar style="auto" />
     </View>
   );
