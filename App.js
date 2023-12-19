99bbe3a5b7b1ca69c83a659c8ac0b7908be2300e
AlessandroSalomonMac <>
2023-12-19 20:54:01 +0100

    ende 70 leere zitate ersatz text anzeigen

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { useState, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -9,23 +9,23 @@ import Bigbutton from './components/Bigbutton'
 import Iconbutton from './components/Iconbutton'
 
 
-
-const zitateData = [
-  { text: "hallo", autor: "ich" },
-  { text: "jeanette", autor: "liebe" },
-  { text: "was möchtest du zu Weihnachten ?", autor: "dich" },
-]
-
 export default function App() {
 
   const [index, setIndex] = useState(0)
-  const [zitate, setZitate] = useState(zitateData)
+  const [zitate, setZitate] = useState([])
   const [isShowDialog, setShowDialog] = useState(false)
-  const zitat = zitate[index]
 
   useEffect(() => {
     loadZitate()
   }, [])
+
+  function setZitatContent() {
+    const zitat = zitate[index]
+    if (zitate.length <= 0) {
+      return <Text style={styles.keineZitate}>Keine Zitate</Text> 
+    }
+    return <Zitate text={zitat.text} autor={zitat.autor} />
+  }
 
   function addZitatToZitateData(text, autor) {
     setShowDialog(false)
@@ -74,7 +74,8 @@ export default function App() {
         onCancel={() => setShowDialog(false)}
         onSave={addZitatToZitateData}>
       </NeueZitate>
-      <Zitate text={zitat.text} autor={zitat.autor} />
+
+      {setZitatContent()}
 
       <Bigbutton style={[styles.button, styles.next]} titel={"next"}
         onPress={() => setIndex((index + 1) % zitate.length)}></Bigbutton>
@@ -112,5 +113,8 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 60,
     left: 20
+  },
+  keineZitate: {
+    fontSize: 50
   }
 });
